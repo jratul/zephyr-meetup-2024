639f9da79c0b2257494e92c6fc78fb82689773c5
jratul <jratul@nate.com>
2024-08-11 22:35:34 +0900

    add intro page

---
+++ src/app/(sections)/Intro.tsx
@@ -0,0 +1,7 @@
+export default function Intro() {
+  return (
+    <div className="h-full flex justify-center align-middle">
+      <div>Zephyr Meetup 2024</div>
+    </div>
+  );
+}
--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -16,7 +16,7 @@ export default function Nav({ curMenuIdx, handleChangeMenu }: Props) {
           {menus.map((menu, menuIdx) => (
             <li
               className={clsx(
-                "hover:text-yellow-300 cursor-pointer my-2",
+                "hover:text-yellow-300 cursor-pointer my-2 font-tenada",
                 menuIdx === curMenuIdx && "text-yellow-500"
               )}
               onClick={() => handleChangeMenu(menuIdx)}
